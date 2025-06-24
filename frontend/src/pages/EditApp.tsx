5a5cea6da89bf1a97e5b09770869677faca0e95b
John Kilroy <55547547+John-Kilroy@users.noreply.github.com>
2025-06-24 12:14:38 -0400

    Create EditApp.tsx

---
+++ frontend/src/pages/EditApp.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import LoginPage from './pages/LoginPage.tsx';
+import CardPage from './pages/CardPage.tsx';
+function App()
+{
+  return (
+    <CardPage />
+  );
+}
+export default App;
