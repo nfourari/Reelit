b72d6d83ee8cc921c98cda148ce7f13424dbb2da
John Kilroy <55547547+John-Kilroy@users.noreply.github.com>
2025-06-24 12:16:37 -0400

    Update EditApp.tsx

--- frontend/src/pages/EditApp.tsx
+++ frontend/src/pages/EditApp.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-
+dom';
 import './App.css';
-import LoginPage from './pages/LoginPage.tsx';
-import CardPage from './pages/CardPage.tsx';
-function App()
-{
+import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
+function App() {
   return (
-    <CardPage />
+    <Router >
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/cards" exact>
+          <CardPage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 export default App;
