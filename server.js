93ef5221195dda96c116fa42a20ce23cf8381ef1
Thu Do <th202814@ucf.edu>
2025-06-23 23:20:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ import mongoose from 'mongoose';
 import express from 'express';
 import cors from 'cors';
 
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-
-
 const app = express();
 
 // Middleware
@@ -20,28 +14,14 @@ mongoose.connect(mongoUri)
   .then(() => console.log('🗄️  MongoDB connected'))
   .catch(err => console.error(err));
 
-// app.use((req, res, next) => 
-// {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PATCH, DELETE, OPTIONS'
-//   );
-//   next();
-// });
-
 // Example endpoint
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK' });
 });
 
 // Start server
+ // start Node + Express server on port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
-// app.listen(5000); // start Node + Express server on port 5000
