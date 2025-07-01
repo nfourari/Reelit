84ec7385973c5560983787ca6b8cacfdfec4a0b0
Thu Do <th202814@ucf.edu>
2025-07-01 11:37:00 -0400

    Update vite.config.ts

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      // whenever the browser requests /api/* on port 5173,
+      // forward it to http://localhost:5001/api/*
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  }
 })
