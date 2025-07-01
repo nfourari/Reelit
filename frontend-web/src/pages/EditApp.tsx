7c86dbea95e9db232e915e83464091cc884c35fe
Thu Do <th202814@ucf.edu>
2025-07-01 12:30:12 -0400

    Update EditApp.tsx

--- frontend-web/src/pages/EditApp.tsx
+++ frontend-web/src/pages/EditApp.tsx
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-
-dom';
-import './App.css';
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
-function App() {
-  return (
-    <Router >
-      <Switch>
-        <Route path="/" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/cards" exact>
-          <CardPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
-}
-export default App;
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import './App.css';
+// import LoginPage from './pages/LoginPage';
+// import CardPage from './pages/CardPage';
+// function App() {
+//   return (
+//     <Router >
+//       <Routes>
+// +       <Route path="/" element={<LoginPage />} />
+// +       <Route path="/cards" element={<CardPage />} />
+// +       <Route path="*" element={<Navigate to="/" replace />} />
+// +     </Routes>
+//     </Router>
+//   );
+// }
+// export default App;
