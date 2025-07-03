ed34f5a9b2d5f0c2d465526e5ff32a3196e40c43
Thu Do <th202814@ucf.edu>
2025-07-03 00:20:05 -0400

    Comment out unused imports

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import LoginPage from './pages/LoginPage.tsx'
