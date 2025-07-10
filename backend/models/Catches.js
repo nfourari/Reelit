2f462fa17e7b56debd03b16059661f5a9493c35d
Alec Baker <alecbakr@gmail.com>
2025-07-10 14:10:49 -0400

    Catches input for mongo

---
+++ backend/models/Catches.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const catchSchema = new mongoose.Schema({
+  userId:  { type: String, required: true },
+  catchName: { type: String, required: true },
+  catchWeight:  { type: float, required: true },
+  catchLength:  { type: float, required: true },
+  catchLocation: { type: String, required: true},
+  catchComment: { type: String, required: false}
+});
+
+
+module.exports = mongoose.model('Catches', catchSchema, 'Catches');
