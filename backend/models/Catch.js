9e225b1a58790fc060e2b412bbc15ed25f3a59fd
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-07-15 10:36:41 -0400

    Update Catch.js
    made FIXs

--- backend/models/Catch.js
+++ backend/models/Catch.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const catchSchema = new mongoose.Schema({
-  userId:  { type: String, required: true },
-  catchName: { type: String, required: true },
-  catchWeight:  { type: float, required: true },
-  catchLength:  { type: float, required: true },
-  catchLocation: { type: String, required: true},
-  catchComment: { type: String, required: false}
+  userId:        { type: String, required: true },
+  catchName:     { type: String, required: true },
+  catchWeight:   { type: Number, required: true },  // <- fixed
+  catchLength:   { type: Number, required: true },  // <- fixed
+  catchLocation: { type: String, required: true },
+  catchComment:  { type: String, required: false }
 });
-
 
 module.exports = mongoose.model('Catch', catchSchema, 'Catches');
