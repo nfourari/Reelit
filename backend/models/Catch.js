e2f61c20bdc2ca857b57b456770881e4b6a7e36e
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-07-20 17:31:13 -0400

    Update Catch.js

--- backend/models/Catch.js
+++ backend/models/Catch.js
@@ -1,14 +1,14 @@
 import { mongoose } from 'mongoose';
 
 const catchSchema = new mongoose.Schema({
-  userId:         { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // <- new
+  userId:         { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   catchName:      { type: String, required: true },
-  catchWeight:    { type: Number, required: true },  // <- fixed
-  catchLength:    { type: Number, required: true },  // <- fixed
+  catchWeight:    { type: Number, required: true },
+  catchLength:    { type: Number, required: true },
   catchLocation:  { type: String, required: true },
   catchComment:   { type: String, required: false },
   caughtAt:       { type: Date, default: Date.now },
-  imageUrl:       { type: String, required: false }
+  imageUrl:       { type: String }
 }, { timestamps: true });
 
 const Catch = mongoose.model('Catch', catchSchema, 'Catches');
