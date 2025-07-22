bca0cee1aa1338361b49181e53a03ae2a5c53b94
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-07-22 10:32:17 -0400

    Update catchesRouter.js

--- backend/routes/catchesRouter.js
+++ backend/routes/catchesRouter.js
@@ -1,4 +1,3 @@
-
 // Groups all catch-related enpoints under /api/catches and ensures only a
 // logged-in user can use them
 
@@ -9,11 +8,13 @@ import {
   listCatches
 } from '../controllers/catchController.js'
 import authenticateUser from '../middleware/authenticateUser.js';
+import upload from '../middleware/upload.js'
 
 
 const catchesRouter = Router();
 
-catchesRouter.post('/',  authenticateUser, createCatch);
+catchesRouter.post('/',  authenticateUser, upload.single('image'), createCatch);
 catchesRouter.get('/',   authenticateUser, listCatches);
 
 export default catchesRouter;
+~                              
