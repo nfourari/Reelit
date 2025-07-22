1974ac16e9bab8e51a3a571423479be215b08e58
AndresOjedaSainz <aojedasainz30@gmail.com>
2025-07-22 10:58:56 -0400

    Update cloudinary.js

--- backend/services/cloudinary.js
+++ backend/services/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log('🔍 CLOUD NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUD NAME:', process.env.CLOUDINARY_CLOUD_NAME);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
